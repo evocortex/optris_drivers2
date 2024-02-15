ed9b97f3f180ebbe517f19ba0c1a204e28a06c6d
maoyangchien <my@shinfangglobal.com>
2024-02-15 11:59:33 +0800

    Render temperture information to image
    Using OpenCV to render bufferimage with highest and lowest temperture to image

--- src/OptrisColorconvert.cpp
+++ src/OptrisColorconvert.cpp
@@ -1,5 +1,9 @@
 #include "OptrisColorconvert.h"
 
+#include <opencv2/opencv.hpp>
+#include <opencv2/imgproc.hpp> // For putText
+#include <opencv2/highgui.hpp> // For imshow, waitKey
+
 namespace optris_drivers2
 {
 
@@ -88,7 +92,7 @@ namespace optris_drivers2
 
   void OptrisColorconvert::onThermalDataReceive(const sensor_msgs::msg::Image::ConstSharedPtr & image)
   {
-    RCLCPP_INFO(get_logger(), "Received new thermal image");
+    //RCLCPP_INFO(get_logger(), "Received new thermal image");
 
     // check for any subscribers to save computation time
     //if(_pubThermal.getNumSubscribers() == 0)
@@ -101,6 +105,46 @@ namespace optris_drivers2
       _bufferThermal = new unsigned char[image->width * image->height * 3];
 
     _iBuilder.convertTemperatureToPaletteImage(_bufferThermal, true);
+
+    //get the max and min temperature
+    int radius = 3;
+    if(image->width <9 || image->height<9) radius = 2;
+    evo::ExtremalRegion minRegion;
+    evo::ExtremalRegion maxRegion;
+    _iBuilder.getMinMaxRegion(radius, &minRegion, &maxRegion);
+    RCLCPP_INFO(get_logger(), "Max temp: %f degree, Min temp: %f degree", maxRegion.t, minRegion.t);
+    
+    // 假設 _bufferThermal 是您已經有的包含圖像數據的buffer
+    // 並且您已經有了 maxRegion.t 和 minRegion.t 這兩個溫度值
+
+    // 首先，將_bufferThermal轉換成OpenCV的Mat格式以便操作
+    cv::Mat thermalImage = cv::Mat(image->height, image->width, CV_8UC3, _bufferThermal);
+
+    // 設置文字參數
+    int fontFace = cv::FONT_HERSHEY_SIMPLEX;
+    double fontScale = 1;
+    int thickness = 2;
+    cv::Scalar textColor(255, 255, 255); // 白色文字
+    int baseline = 0;
+
+    // 準備要顯示的文字
+    std::string maxTempText = "Max Temp: " + std::to_string(maxRegion.t) + " C";
+    std::string minTempText = "Min Temp: " + std::to_string(minRegion.t) + " C";
+
+    // 獲取文字框大小
+    cv::Size textSizeMax = cv::getTextSize(maxTempText, fontFace, fontScale, thickness, &baseline);
+    cv::Size textSizeMin = cv::getTextSize(minTempText, fontFace, fontScale, thickness, &baseline);
+
+    // 選擇文字位置
+    cv::Point maxTempPos(10, textSizeMax.height + 10); // 略高於圖像底部
+    cv::Point minTempPos(10, textSizeMax.height + textSizeMin.height + 20); // 在最高溫度資訊下方
+
+    // 在圖像上繪製文字
+    cv::putText(thermalImage, maxTempText, maxTempPos, fontFace, fontScale, textColor, thickness);
+    cv::putText(thermalImage, minTempText, minTempPos, fontFace, fontScale, textColor, thickness);
+
+    // 將修改後的圖像 data 回寫到_bufferThermal
+    memcpy(_bufferThermal, thermalImage.data, image->height * image->width*3*sizeof(unsigned char));
 
     sensor_msgs::msg::Image img;
     img.header.frame_id = "thermal_image_view";
@@ -117,13 +161,7 @@ namespace optris_drivers2
     sensor_msgs::msg::CameraInfo camera_info = _camera_info_manager.getCameraInfo();
     camera_info.header = img.header;
     _pubThermal.publish(img, camera_info);
-    //get the max and min temperature
-    int radius = 3;
-    if(img.width <9 || img.height<9) radius = 2;
-    evo::ExtremalRegion minRegion;
-    evo::ExtremalRegion maxRegion;
-    _iBuilder.getMinMaxRegion(radius, &minRegion, &maxRegion);
-    RCLCPP_INFO(get_logger(), "Max temp: %f degree, Min temp: %f degree", maxRegion.t, minRegion.t);
+    
   }
 
   void OptrisColorconvert::onVisibleDataReceive(const sensor_msgs::msg::Image::ConstSharedPtr & image)
